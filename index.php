8bcdca29e41635379745cc1c58a200edcb0941c8
cristhian <cristhyan_2011@hotmail.com>
2018-11-22 19:13:10 -0600

    Primer login

--- index.php
+++ index.php
@@ -5,6 +5,22 @@
 		<title>Itsco libros</title>
 	</head>
 	<body>
-
+		<center>
+		<form>
+			<table>
+				<tr>
+				<td> Usuario </td>
+				<td> <input type="text" name="txtuser"></td>
+				</tr>
+				<tr>
+					<td> Contraseña</td>
+					<td> <input type="password" name="txtcontra"></td>
+				</tr>
+				<tr>
+					<td> <input type="Submit" value="Ingresar"></td>
+				</tr>
+			</table>
+		</form>
+		</center>
 	</body>
 </html>
