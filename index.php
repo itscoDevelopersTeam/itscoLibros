747b7b36512003565a76f5557a51ca87eac4913d
Samuel Gómez Balderas <sierra_034@outlook.com>
2018-11-30 01:28:06 -0600

    Diseño login fontawesome

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
+
 <html>
 	<head>
-		<title>Itsco libros</title>
-		<link rel="stylesheet" type="text/css" href="css/estiloLogin.css" />
+		<title>itscoLibros</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/css/all.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">	
 	</head>
-<body id="loginForm">
-<center>
-	
-	<form id ="formMain">
-	<div><h3> Ingresa tus datos </h3></div>
-		<div><input type="text" name="txtcorreo" class="cajas" placeholder="ingresa correo"></div>
-		<br><br>
-		<div><input type="password" name="txtcontraseña" class="cajas" placeholder="ingresa password"></div>
-		<br>
-		<div><a href= "php/Paneles/usuarioAdmin.php" id="botonEntrar"> Entrar </a></div>
-	</form>
-</center>
-</body>
+
+	<body>
+		<form>
+			<div class="form-row login-header">
+				<i class="fas fa-graduation-cap fa-3x"></i>
+				<h4>itscoLibros</h4>
+			</div>
+
+			<!-- Campos de datos -->
+			<div class="form-row">
+				<input type="text" id="txtUsuario" placeholder="Nombre usuario">
+			</div>
+
+			<div class="form-row">
+				<input type="password" id="txtPassword" placeholder="Contraseña">
+			</div>
+
+			<div class="form-row">
+				<input type="submit" value="ingresar">
+			</div>
+		</form>
+	</body>
 </html>
